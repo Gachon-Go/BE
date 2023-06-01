c930f33c4f52433f00056312bc2a3b1c469d0821
peter <97747863+wcorn@users.noreply.github.com>
2023-06-01 14:00:38 +0900

    hotfix: 내 위치 정보 안가져오기

--- service/src/main/java/gcu/mp/service/map/MapServiceImpl.java
+++ service/src/main/java/gcu/mp/service/map/MapServiceImpl.java
@@ -15,10 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -60,6 +57,9 @@ public class MapServiceImpl implements MapService {
                     .mapPointDtoList(mapPointDtoList).build();
         }
         for (Member member : memberList) {
+            if(Objects.equals(member.getId(), memberId)){
+                continue;
+            }
             String data = redisUtil.getData("naverMap " + member.getId());
             MapPointDto mapPointDto;
             if (data==null) {
